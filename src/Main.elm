8a90af886cdff98cccd07a8e2711dcd6128d3b21
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2023-01-24 10:49:16 +0000

    Beginnings.

--- src/Main.elm
+++ src/Main.elm
@@ -19,10 +19,6 @@ import Url exposing (Url)
 import Url.Parser exposing (Parser, map, oneOf, parse, s, top)
 
 
-
---TODO: See if we can host on WebMate.me
-
-
 type alias Model =
     { active : Maybe Entry
     , entries : List Entry
@@ -100,11 +96,7 @@ view model =
 notes =
     column [ spacing 10, centerX, padding 20 ] <|
         List.map (paragraph [] << List.singleton)
-            [ text "Most cycling clubs now feature “half-wheeling” as the Number One thing not to do on a group ride. "
-            , text "The Gregarios were one of the very first clubs to feature a Ride Etiquette Page on a club website. "
-            , text "Much of our Etiquette content has been adopted by other clubs up and down the country."
-            , text "Our Paceline and Chaingang instructions have been copied by many clubs."
-            ]
+            [ text "Routes." ]
 
 
 type Route
@@ -144,191 +136,17 @@ buyMeACoffeeButton =
 type alias Entry =
     { title : String
     , content : String
-
-    --, picture : String
+    , gpx : String
     }
 
 
 content : List Entry
 content =
-    [ Entry
-        "Riding in Pairs"
-        """Where space allows we ride in two tidy lines. 
-The front positions will change regularly so everyone gets a chance to ride on the front
-and share the pace-making.
-
-[This video](https://www.youtube.com/watch?v=-9upR9r2_uQ) has a great explanation of how we rotate positions.
-        """
-    , Entry
-        "Don’t be all over the road"
-        """If you’re in the outside line keep close to the rider alongside and don’t stray into the 
-middle of the road (for obvious reasons!!).
-Keeping a tight group or _gruppo compatto_ not only looks good,
-it’s safer, and it saves you a big chunk of energy by merely riding in the slipstream ."""
-    , Entry
-        "Stay on the tracks"
-        """When riding double hold your line and don’t veer or wobble about.
-Just imagine you’re on railway tracks holding the same distance between the rider next to you."""
-    , Entry
-        "Try not to switch lanes or positions"
-        """If the group stops, comes to a junction or roundabout, or singles out, always try and resume your position.
-It’s all about keeping it predictable. This also enables everyone to have the same opportunity to
-contribute a turn on the front with the pace-making, and no one is popping up anywhere unexpected."""
-    , Entry
-        "Don’t be a half-wheeler"
-        """When riding at the front, try and make sure you ride level with rider next to you, and at the same pace.
-Riding slightly in front of your opposite rider will push your partner to “catch up”,
-increasing the speed of the whole group.
-This is termed “half-wheeling” and it disrupts the rhythm of the group.
-A great way to check whether you’re riding level is to ride brake hood to brake hood.
-If you look to the side and there’s no-one there then you need to ease back!"""
-    , Entry
-        "Junctions"
-        """Members of the group who have negotiated a junction should be aware that other members
-may have had to wait and need time to rejoin so should proceed slowly and check back
-before getting back on pace. Lead riders can indicate whether it is clear of traffic,
-but everyone is responsible for their own safety and must take individual care."""
-    , Entry
-        "Out of the saddle"
-        """As a hill gets steeper you may wish to get out of the saddle.
-Make sure you don’t kickback as you get out of the saddle.
-Best bet is to change up a gear or two (to give yourself something to push against)
-and make sure your pedal is at a 2pm position so that you can stand on it,
-then just keep the movements smooth. It’s worth practicing this on your own."""
-    , Entry
-        "Communicate potholes and hazards"
-        """Communication, anticipation, and awareness are important for safe group rides.
-The front riders should look well ahead and give a clear warning about potholes
-and other hazards well before we pass them.
-Front riders in the outside lane should also be looking out for potholes in front
-of the inside rider - anticipate and react if the rider on your inside may need to swing out.
-Approaching a pothole plan a shallow line round it that the riders behind can easily follow -
-try not to flick round them at the last minute!
-Everyone is responsible for relaying forward any warnings through the group,
-rather than having to rely on muffled shouts from the back or front."""
-    , Entry
-        "Cars coming past"
-        """Yes, we already know there are cars on the road! It’s expected!
-We don’t need to be yelling just for the sake of it.
-As long as everyone holds their line, there should be no need to yell every time a
-car comes past unless for some reason they are endangering the group.
-If there is a queue of cars behind, we should and usually do single out.
-The terms “car front” and “car back” work well when shouting might be necessary."""
-    , Entry
-        "Pacing the ride"
-        """We aim to ride at a pace that is comfortable for all riders in the group.
-The front riders will be working a bit harder… as they’re on the front,
-and they should ride on effort, not speed.
-
-The effort level should be as constant as possible.
-This means that the speed will be faster on the flat with appropriate gear changes,
-and slower on the hills and rises again with appropriate use of your gears.
-We’re not trying to maintain a constant speed, because the terrain will change and also the wind conditions.
-
-The two front riders set and maintain the pace, but the rest of the group are
-responsible for indicating if this pace exceeds the comfort threshold of any member.
-If you notice that the rider next to you is struggling to keep up,
-or a rider at the back has fallen behind the ride, a shout of “mile off” or
-“half a mile off” will let the front riders know that they need to drop the pace slightly.
-
-The skill of setting the pace is to ensure the group isn’t splitting every time
-there’s a rise in the road, but we may need to regroup at the top of any significant hill -
-especially if some riders are going for hilltop points.
-This should not be happening on every little rise."""
-    , Entry
-        "Light on the brakes"
-        """For a safe enjoyable ride it’s important to ride smoothly, and try and avoid hard
-braking or swerving as much as possible. Try and slow by feathering the brakes
-lightly or even just moving out into the wind to take off a little speed.
-
-**Stay alert** at all times and keep “reading” the dynamics of the group. """
-    , Entry
-        "Hold your line"
-        """This means swerving as little as possible.
-Always ride in a straight line, and keep your pace as even as possible with shoulders and arms relaxed.
-Don’t ride in the middle of the road unless it’s a small country lane.
-If you’re on a small country lane it’s best to single out before going into a blind corner!"""
-    , Entry
-        "Don’t overlap wheels"
-        """Overlapping, is putting your front wheel next to someone’s rear wheel.
-Try to always be behind the bike in front unless you’re passing.
-It’s a good idea to be very slightly offset to left or right,
-then if the rider in front stops you will still have a safety margin and wont ride directly into them."""
-    , Entry
-        "Relax!"
-        """Your bike will be more stable and easy to control when you’re relaxed.
-Use a relaxed grip on the handlebars, keep your shoulders down (not up against your neck)
-and bend your elbows slightly.
-These steps will help you stay relaxed, which allows quicker reaction time and prevents
-tension in the neck and shoulders that can lead to fatigue and sloppy riding."""
-    , Entry
-        "Focus ahead"
-        """Look up towards the front of the group and beyond so you can see what’s going on
-at the head of the group - and in front of the group.
-It’s termed “soft focus” - you’re looking to the front of the group yet
-you will still be aware of what’s happening directly in front of you.
-It prepares you for any sudden changes."""
-    , Entry
-        "Anticipate"
-        """Anticipate when the group might be slowing for a junction, bend in the road, traffic lights, etc.
-Anticipate when you might need to change gear."""
-    , Entry
-        "Keep it smooth"
-        """Pedal smoothly and evenly to save energy and make it easier for the riders behind you."""
-    , Entry
-        "Don’t look back"
-        """(unless it’s really necessary) Focus ahead where you are going."""
-    , Entry
-        "In the wet"
-        """Proper mudguards with long flaps will win you friends in the peloton!
-Moderate the speed. Just knock it back and be safe.
-Try to avoid going over white painted road markings while turning as they can be  slippery ...
-especially white domed roundabouts!!!
-The same applies to metalwork on the road ... OK in straight line but if possible try not to
-be turning and leaning the bike on paintwork or metal covers.
-Make sure your mudflap is long enough to protect the rider behind you."""
-    , Entry
-        "Horses"
-        """When overtaking horses either solo or in a group the lead riders should shout a clear
-audible warning like “coming through” while slowing down while and also giving them plenty of room.
-Both rider and horse will appreciate this!"""
-    , Entry
-        "Downhill"
-        """Descend on the drops if it’s a significant or proper hill.
-This lowers your centre of gravity, gives you more control, improved pull on the brakes,
-and it makes you faster!  At speed being on the drops will keep you in control of the
-bike if you hit a pothole ... on the brake hoods any impact could cause you to lose your grip.
-You will catch riders in front if you’re in their slipstream,
-so just move out into the wind to take off a bit of speed.
-
-**Downhill cornering check list**
-- Weight on the outside foot.
-- Head up… like a periscope while the bike leans.
-- Look towards your exit point. Relax.
-
-**Downhill Fast Straights**
-
-Bumpy or uneven surface? Try this ... cranks horizontal, weight
-increased on the pedals so you’re almost standing on them.
-Experiment with these techniques."""
-    , Entry
-        "Awareness"
-        """Always pay attention to what’s happening on the road ahead of the group,
-and also the general group dynamics. Knock it back a bit through busy urban areas."""
-    , Entry
-        "Pre Ride"
-        """Inspect tyres and remove any small flints that are embedded."""
-    , Entry
-        "At the start"
-        """You’re rocking up to the start and see a rider or two that are not in club kit.
-Probably it’s their first ride or they’re fairly new members.
-It’s a Gregarios’ tradition to give them the same friendly welcome that was extended to you.
-Please don’t pretend not to notice them, that’s just not cool."""
-    , Entry
-        "The three most important things to remember"
-        """- be predictable
-- be predictable!
-- be predictable!!"""
+    [ Entry "Windsor"
+        """This popular shortish route skims the southern edge of the Chilterns
+ before taking to flatter and faster roads including the famour "Museeuw Lane" before
+ rolling into Windsor for some signature Cinnamon buns at the reknowned Cinnamon Cafe."""
+        "Windsor.gpx"
     ]
 
 
@@ -395,7 +213,6 @@ tricolore width =
 
 entryDetail : Entry -> Element Msg
 entryDetail entry =
-    -- Try to get tricolore border.
     let
         closeButton =
             Input.button
